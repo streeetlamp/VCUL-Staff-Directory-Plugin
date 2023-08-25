dc56c8a46cb2f93e926ccb6b34fb7aa0e6285c51
cody <hi@codywhitby.net>
2023-08-25 11:26:50 -0400

    directory contributor role

--- includes/directory-contributor-role.php
+++ includes/directory-contributor-role.php
@@ -3,71 +3,71 @@
 namespace VCUL\Directory\Contributor_Role;
 
 /**
- * Provides the name of the Scholarship Contributor role.
+ * Provides the name of the Directory Contributor role.
  *
  * @since 0.1.0
  *
  * @return string
  */
-function scholarship_contributor() {
-	return 'wsuwp_scholarship_contributor';
+function directory_contributor() {
+	return 'vcul_directory_contributor';
 }
 
-add_action( 'init', 'VCUL\Directory\Contributor_Role\add_scholarship_contributor_role' );
+add_action( 'init', 'VCUL\Directory\Contributor_Role\add_directory_contributor_role' );
 /**
- * Adds the Scholarship Contributor role.
+ * Adds the Directory Contributor role.
  *
  * @since 0.1.0
  */
-function add_scholarship_contributor_role() {
-	if ( array_key_exists( scholarship_contributor(), \WP_Roles()->get_names() ) ) {
+function add_directory_contributor_role() {
+	if ( array_key_exists( directory_contributor(), \WP_Roles()->get_names() ) ) {
 		return;
 	}
 
 	add_role(
-		scholarship_contributor(),
-		'Scholarship Contributor',
+		directory_contributor(),
+		'Directory Contributor',
 		array(
-			'create_scholarships' => true,
-			'edit_scholarships' => true,
+			'create_directory' => true,
+			'edit_directory' => true,
 			'read' => true,
 			'upload_files' => true,
 		)
 	);
 }
 
-add_action( 'init', 'VCUL\Directory\Contributor_Role\map_scholarship_contributor_capabilities', 13 );
+add_action( 'init', 'VCUL\Directory\Contributor_Role\map_directory_contributor_capabilities', 13 );
 /**
- * Maps the Scholarship Contributor role capabilities to the scholarship post type.
+ * Maps the Directory Contributor role capabilities to the directory post type.
  *
  * @since 0.1.0
  */
-function map_scholarship_contributor_capabilities() {
+function map_directory_contributor_capabilities() {
 	$user = wp_get_current_user();
 
-	if ( ! in_array( scholarship_contributor(), (array) $user->roles, true ) ) {
+	if ( ! in_array( directory_contributor(), (array) $user->roles, true ) ) {
 		return;
 	}
 
-	$scholarships = get_post_type_object( \VCUL\Directory\Post_Type\post_type_slug() );
+	$directory = get_post_type_object( \VCUL\Directory\Post_Type\post_type_slug() );
 
-	if ( $scholarships ) {
-		$scholarships->cap->create_posts = 'create_scholarships';
-		$scholarships->cap->edit_posts = 'edit_scholarships';
+	if ( $directory ) {
+		$directory->cap->create_posts = 'create_directory';
+		$directory->cap->edit_posts = 'edit_directory';
 	}
 
 	$taxonomies = get_taxonomies( array(), 'objects' );
 
 	if ( $taxonomies ) {
 		foreach ( $taxonomies as $taxonomy ) {
-			$taxonomy->cap->assign_terms = 'edit_scholarships';
+			$taxonomy->cap->assign_terms = 'edit_directory';
 		}
 	}
 }
 
 add_action( 'pre_get_posts', 'VCUL\Directory\Contributor_Role\filter_list_tables' );
 /**
- * Filters the media library view for users with the Scholarship Contributor role.
+ * Filters the media library view for users with the Directory Contributor role.
  *
  * @since 0.1.0
  *
@@ -80,7 +80,7 @@ function filter_list_tables( $query ) {
 
 	$user = wp_get_current_user();
 
-	if ( ! in_array( scholarship_contributor(), (array) $user->roles, true ) ) {
+	if ( ! in_array( directory_contributor(), (array) $user->roles, true ) ) {
 		return;
 	}
 
