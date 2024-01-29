721c321a9db9664255d816937b701d13fafc2101
cody <hi@codywhitby.net>
2024-01-29 12:45:11 -0500

    org chart

--- plugin.php
+++ plugin.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: VCUL Staff Directory
-Version: 0.0.28
+Version: 0.0.29
 Description: A WordPress plugin for managing a staff directory.
 Author: VCUL Web Team
 Author URI: https://library.vcu.edu
